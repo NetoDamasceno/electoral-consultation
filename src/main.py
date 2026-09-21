eeb41fb366114d03d18f606e487c193e343e726e
NetoDamasceno <luiznetto13@gmail.com>
2026-09-20 22:43:26 -0300

    initial commit

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+
+    page = browser.new_page()
+
+    page.goto(
+        "https://www.tse.jus.br/servicos-eleitorais/autoatendimento-eleitoral"
+        "?utm_source=chatgpt.com"
+        "#/atendimento-eleitor/consultar-situacao-titulo-eleitor"
+    )
+
+    campo_identificacao = page.get_by_placeholder(
+        "Número do título eleitoral ou CPF ou nome"
+    )
+
+    campo_identificacao.fill("LUIZ ANTONIO SOARES DAMASCENO NETO")
+
+    campo_identificacao.press("Tab")
+
+    page.wait_for_timeout(500)
+
+    page.keyboard.type("13/07/2005")
+
+    page.keyboard.press("Enter")
+
+    page.wait_for_timeout(5000)
+
+    input("Pressione ENTER para fechar o navegador...")
+
+    browser.close()
